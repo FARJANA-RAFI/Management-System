72e68b23a6db8a8483b5f019961d8cea4eacbeb4
FARJANA-RAFI <69666670+FARJANA-RAFI@users.noreply.github.com>
2023-10-24 11:12:32 +0600

    Add files via upload

---
+++ database/migrations/2021_05_19_062806_create_enrolled_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrolledCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrolled_courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_name')->nullable();
+            $table->string('student_id')->nullable();
+            $table->string('session')->nullable();
+            $table->string('department')->nullable();
+            $table->string('semester')->nullable();
+            $table->string('course')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrolled_courses');
+    }
+}
